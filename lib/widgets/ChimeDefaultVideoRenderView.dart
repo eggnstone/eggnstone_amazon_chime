6ac944e33cb16a78fe5162eb7ca2b2cd5892dfb5
Mark Eggenstein <>
2020-08-03 15:47:19 +0200

    ChimeDefaultVideoRenderView cleanup

--- lib/widgets/ChimeDefaultVideoRenderView.dart
+++ lib/widgets/ChimeDefaultVideoRenderView.dart
@@ -2,32 +2,39 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-class ChimeDefaultVideoRenderView extends StatefulWidget {
-  final ValueChanged<int> onPlatformViewCreated;
+class ChimeDefaultVideoRenderView extends StatefulWidget
+{
+    final ValueChanged<int> onPlatformViewCreated;
 
-  ChimeDefaultVideoRenderView({this.onPlatformViewCreated});
+    ChimeDefaultVideoRenderView({this.onPlatformViewCreated});
 
-  @override
-  _ChimeDefaultVideoRenderViewState createState() =>
-      _ChimeDefaultVideoRenderViewState();
+    @override
+    _ChimeDefaultVideoRenderViewState createState()
+    => _ChimeDefaultVideoRenderViewState();
 }
 
-class _ChimeDefaultVideoRenderViewState
-    extends State<ChimeDefaultVideoRenderView> {
-  @override
-  Widget build(BuildContext context) {
-    if (Platform.isAndroid) {
-      return AndroidView(
-        viewType: 'ChimeDefaultVideoRenderView',
-        onPlatformViewCreated: (int viewId) =>
-            widget.onPlatformViewCreated?.call(viewId),
-      );
-    } else {
-      return UiKitView(
-        viewType: 'ChimeDefaultVideoRenderView',
-        onPlatformViewCreated: (int viewId) =>
-            widget.onPlatformViewCreated?.call(viewId),
-      );
+class _ChimeDefaultVideoRenderViewState extends State<ChimeDefaultVideoRenderView>
+{
+    @override
+    Widget build(BuildContext context)
+    {
+        if (Platform.isAndroid)
+        {
+            return AndroidView(
+                viewType: 'ChimeDefaultVideoRenderView',
+                onPlatformViewCreated: (int viewId)
+                => widget.onPlatformViewCreated?.call(viewId),
+            );
+        }
+        else if (Platform.isIOS)
+        {
+            return UiKitView(
+                viewType: 'ChimeDefaultVideoRenderView',
+                onPlatformViewCreated: (int viewId)
+                => widget.onPlatformViewCreated?.call(viewId),
+            );
+        }
+        else
+            throw Exception('Not implemented');
     }
-  }
 }
