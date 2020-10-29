d76062f11e4e8d5506f33ff965b54646ad0e520c
Mark Eggenstein <>
2020-10-29 17:41:38 +0100

    Potential fix for OnActiveSpeakerDetected on Android.

--- android/src/main/kotlin/dev/eggnstone/chime/observers/ChimeActiveSpeakerObserver.kt
+++ android/src/main/kotlin/dev/eggnstone/chime/observers/ChimeActiveSpeakerObserver.kt
@@ -1,27 +1,31 @@
 package dev.eggnstone.chime.observers
 
-import android.os.Looper
+import android.os.Handler
 import android.os.Looper.getMainLooper
 import com.amazonaws.services.chime.sdk.meetings.audiovideo.AttendeeInfo
 import com.amazonaws.services.chime.sdk.meetings.audiovideo.audio.activespeakerdetector.ActiveSpeakerObserver
 import io.flutter.plugin.common.EventChannel
 import org.json.JSONArray
 import org.json.JSONObject
-import java.util.logging.Handler
 
-class ChimeActiveSpeakerDetectedObserver(private val _eventSink: EventChannel.EventSink) : ActiveSpeakerObserver {
+class ChimeActiveSpeakerDetectedObserver(private val _eventSink: EventChannel.EventSink) : ActiveSpeakerObserver
+{
     override val scoreCallbackIntervalMs: Int?
         get() = null
 
-    override fun onActiveSpeakerDetected(attendeeInfo: Array<AttendeeInfo>) {
+    override fun onActiveSpeakerDetected(attendeeInfo: Array<AttendeeInfo>)
+    {
         val jsonObject = JSONObject()
         jsonObject.put("Name", "OnActiveSpeakerDetected")
         jsonObject.put("Arguments", convertVolumeUpdatesToJson(attendeeInfo))
 
-        _eventSink.success(jsonObject.toString())
+        Handler(getMainLooper()).post {
+            _eventSink.success(jsonObject.toString())
+        }
     }
 
-    override fun onActiveSpeakerScoreChanged(scores: Map<AttendeeInfo, Double>) {
+    override fun onActiveSpeakerScoreChanged(scores: Map<AttendeeInfo, Double>)
+    {
         val jsonObject = JSONObject()
         jsonObject.put("Name", "OnActiveSpeakerScoreChanged")
 
