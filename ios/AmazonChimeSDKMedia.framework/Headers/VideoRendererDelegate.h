b7ce629b611ec03f27e06ea7a6455494a1b97e04
henlamk <hendrik.lakaemper@gmail.com>
2020-10-30 08:26:09 +0100

    Add newest iOS SDK version, updated README

--- ios/AmazonChimeSDKMedia.framework/Headers/VideoRendererDelegate.h
+++ ios/AmazonChimeSDKMedia.framework/Headers/VideoRendererDelegate.h
@@ -6,6 +6,21 @@
 //
 #import <AVFoundation/AVFoundation.h>
 
+// Mirrors  https://w3c.github.io/mst-content-hint/
+typedef NS_ENUM(NSUInteger, VideoContentHintInternal) {
+    VideoContentHintInternalNone = 0,
+    VideoContentHintInternalMotion,
+    VideoContentHintInternalDetailed,
+    VideoContentHintInternalText,
+};
+
+typedef NS_ENUM(NSUInteger, VideoRotationInternal) {
+    VideoRotation0 = 0,
+    VideoRotation90 = 90,
+    VideoRotation180 = 180,
+    VideoRotation270 = 270,
+};
+
 typedef NS_ENUM(NSUInteger, PauseState) {
     Unpaused,
     PausedByUserRequest,
@@ -15,8 +30,10 @@ typedef NS_ENUM(NSUInteger, PauseState) {
 @protocol VideoRendererDelegate <NSObject>
 
 - (void)didReceiveVideoBuffer:(CVPixelBufferRef)image
-                     profileId:(NSString*)profileId
-                       videoId:(uint32_t)videoId;
+                    profileId:(NSString*)profileId
+                      videoId:(uint32_t)videoId
+                  timestampNs:(int64_t)timestampNs
+                     rotation:(VideoRotationInternal)rotation;
 
 - (void)didUpdatePauseForRendererWithId:(uint32_t)videoId state:(PauseState)type;
 
