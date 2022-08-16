e66c85d5ad57c1883134e5a1b1a5f820ecb1aad9
eggnstone <eggnstone@users.noreply.github.com>
2022-08-16 19:42:44 +0200

    Fixing formatting.

--- ios/Classes/observers/ChimeDataMessageObserver.swift
+++ ios/Classes/observers/ChimeDataMessageObserver.swift
@@ -1,5 +1,5 @@
 //
-//  ChimeM.swift
+//  ChimeDataMessageObserver.swift
 //  eggnstone_amazon_chime
 //
 //  Created by 長谷川 樹 on 2022/07/25.
@@ -9,26 +9,23 @@ import Foundation
 import AmazonChimeSDK
 import Flutter
 
-public class ChimeDataMessageObserver : DataMessageObserver{
-    
+public class ChimeDataMessageObserver : DataMessageObserver {
     let _eventSink: FlutterEventSink
     
     init(eventSink: @escaping FlutterEventSink) {
         self._eventSink = eventSink
     }
     
-    
     public func dataMessageDidReceived(dataMessage: DataMessage) {
-
         let arguments = """
             {
-            "timestampMs": "\(dataMessage.timestampMs)",
-            "topic": "\(dataMessage.topic)",
-            "senderAttendeeId": "\(dataMessage.senderAttendeeId)",
-            "senderExternalUserId": "\(dataMessage.senderExternalUserId)",
-            "throttled": "\(dataMessage.throttled)",
-            "data": "\(dataMessage.data)",
-            "text": \(dataMessage.text()!)
+            "Data": "\(dataMessage.data)",
+            "SenderAttendeeId": "\(dataMessage.senderAttendeeId)",
+            "SenderExternalUserId": "\(dataMessage.senderExternalUserId)",
+            "Text": \(dataMessage.text()!),
+            "Throttled": "\(dataMessage.throttled)",
+            "TimestampMs": "\(dataMessage.timestampMs)",
+            "Topic": "\(dataMessage.topic)"
             }
             """
 
@@ -41,5 +38,4 @@ public class ChimeDataMessageObserver : DataMessageObserver{
 
         _eventSink(json)
     }
-    
 }
